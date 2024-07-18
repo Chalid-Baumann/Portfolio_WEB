8747bb05f0db1f3bf30d336e1d4e0f6176a0c14b
Chalid-Baumann <chalidbaumann@gmx.ch>
2024-07-18 19:35:57 +0200

    redtypo

--- Jagdschloss/Jagdschloss.js
+++ Jagdschloss/Jagdschloss.js
@@ -34,3 +34,21 @@ document.addEventListener('DOMContentLoaded', () => {
         window.scrollTo({ top: 0, behavior: 'smooth' });
     });
 });
+
+
+//Farbänderung Mobile,Hover
+if ('ontouchstart' in document.documentElement) {
+    // Add event listener for tap on links
+    document.addEventListener('touchstart', function (event) {
+        // Check if the touched element is a link
+        if (event.target.tagName.toLowerCase() === 'a') {
+            // Change text color to rgb(255, 28, 65) on tap
+            event.target.style.color = 'rgb(255, 28, 65)';
+            
+            // Optionally, you can revert the color after a delay
+            setTimeout(function () {
+                event.target.style.color = ''; // Revert to default or specified color
+            }, 1000); // Adjust the delay time as needed (1000ms = 1 second)
+        }
+    });
+}
