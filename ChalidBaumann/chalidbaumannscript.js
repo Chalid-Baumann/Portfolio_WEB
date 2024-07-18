24b921f3e061ce4d081b32a4b5c53321f8599af7
Chalid-Baumann <chalidbaumann@gmx.ch>
2024-07-18 12:19:37 +0200

    Jagdschloss

---
+++ ChalidBaumann/chalidbaumannscript.js
@@ -0,0 +1,61 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const galleryItems = document.querySelectorAll('.gallery-item');
+
+    const arrangeGallery = () => {
+        let lastItemWasVertical = false;
+
+        galleryItems.forEach((item, index) => {
+            const aspectRatio = item.getAttribute('data-aspect-ratio');
+
+            // Entfernen von vorherigen Klassen
+            item.classList.remove('large', 'medium', 'small');
+            
+            // Layout-Logik
+            if (aspectRatio === 'vertical') {
+                item.classList.add('large');
+                lastItemWasVertical = true;
+            } else if (aspectRatio === 'horizontal' && lastItemWasVertical) {
+                item.classList.add('medium');
+                lastItemWasVertical = false;
+            } else if (aspectRatio === 'horizontal' && !lastItemWasVertical) {
+                item.classList.add('small');
+                lastItemWasVertical = false;
+            } else if (aspectRatio === 'square') {
+                item.classList.add('small');
+            }
+        });
+    };
+
+    arrangeGallery();
+    window.addEventListener('resize', arrangeGallery);
+
+    // Scroll-zu-Top-Funktionalität hinzufügen
+    document.getElementById('scrollTop').addEventListener('click', () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+});
+
+
+// Fancybox Initialisierung
+$(document).ready(function() {
+    $('[data-fancybox]').fancybox({
+      buttons: [
+        "zoom",
+        "share",
+        "slideShow",
+        "fullScreen",
+        "download",
+        "thumbs",
+        "close"
+      ],
+      loop: false,
+      protect: true
+    });
+  });
+
+
+
+
+
+
+  
