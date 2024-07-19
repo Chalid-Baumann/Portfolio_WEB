4a2dec9db975e51a6558d52956a8550bb66ef2c1
Chalid-Baumann <chalidbaumann@gmx.ch>
2024-07-19 10:41:09 +0200

    19.07.24

--- ChalidBaumann/chalidbaumannscript.js
+++ ChalidBaumann/chalidbaumannscript.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const galleryItems = document.querySelectorAll('.gallery-item');
+    const imageElement = document.getElementById('chalid-image');
 
     const arrangeGallery = () => {
         let lastItemWasVertical = false;
@@ -30,32 +31,70 @@ document.addEventListener('DOMContentLoaded', () => {
     window.addEventListener('resize', arrangeGallery);
 
     // Scroll-zu-Top-Funktionalität hinzufügen
-    document.getElementById('scrollTop').addEventListener('click', () => {
-        window.scrollTo({ top: 0, behavior: 'smooth' });
+    const scrollTopButton = document.getElementById('scrollTop');
+    if (scrollTopButton) {
+        scrollTopButton.addEventListener('click', () => {
+            window.scrollTo({ top: 0, behavior: 'smooth' });
+        });
+    }
+
+    // Farbänderung Mobile, Hover
+    if ('ontouchstart' in document.documentElement) {
+        document.addEventListener('touchstart', function (event) {
+            if (event.target.tagName.toLowerCase() === 'a') {
+                event.target.style.color = 'rgb(255, 28, 65)';
+                setTimeout(() => {
+                    event.target.style.color = '';
+                }, 1000); // Adjust delay as needed
+            }
+        });
+    }
+
+    // Mousemove-Event-Listener für Desktops
+    document.addEventListener('mousemove', function (e) {
+        if (!imageElement) return; // Überprüfen, ob das Bild-Element vorhanden ist
+
+        const rect = imageElement.getBoundingClientRect();
+        const x = e.clientX - rect.left; 
+        const y = e.clientY - rect.top; 
+        const width = rect.width;
+        const height = rect.height;
+        
+        if (x < width / 3) {
+            imageElement.src = '/images/Chalid_Baumann_1.png';
+        } else if (x > 2 * width / 3) {
+            imageElement.src = '/images/Chalid_Baumann_3.png';
+        } else if (y < height / 3) {
+            imageElement.src = '/images/Chalid_Baumann_2.png';
+        } else if (y > 2 * height / 3) {
+            imageElement.src = '/images/Chalid_Baumann_4.png';
+        } else {
+            imageElement.src = '/images/Chalid_Baumann_5.png';
+        }
+    });
+
+    // Touchmove-Event-Listener für Mobile
+    document.addEventListener('touchmove', function (e) {
+        if (!imageElement) return; // Überprüfen, ob das Bild-Element vorhanden ist
+
+        // Verarbeiten Sie das erste Touch-Event
+        const touch = e.touches[0];
+        const rect = imageElement.getBoundingClientRect();
+        const x = touch.clientX - rect.left; 
+        const y = touch.clientY - rect.top; 
+        const width = rect.width;
+        const height = rect.height;
+
+        if (x < width / 3) {
+            imageElement.src = '/images/Chalid_Baumann_1.png';
+        } else if (x > 2 * width / 3) {
+            imageElement.src = '/images/Chalid_Baumann_3.png';
+        } else if (y < height / 3) {
+            imageElement.src = '/images/Chalid_Baumann_2.png';
+        } else if (y > 2 * height / 3) {
+            imageElement.src = '/images/Chalid_Baumann_4.png';
+        } else {
+            imageElement.src = '/images/Chalid_Baumann_5.png';
+        }
     });
 });
-
-
-// Fancybox Initialisierung
-$(document).ready(function() {
-    $('[data-fancybox]').fancybox({
-      buttons: [
-        "zoom",
-        "share",
-        "slideShow",
-        "fullScreen",
-        "download",
-        "thumbs",
-        "close"
-      ],
-      loop: false,
-      protect: true
-    });
-  });
-
-
-
-
-
-
-  
